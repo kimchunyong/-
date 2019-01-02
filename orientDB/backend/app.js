53e952a17ad8ad1bd4a83fb31c5d1a274b59c4dc
Kimchunyong <lovemy5854@naver.com>
2019-01-02 18:20:32 +0900

    test1

--- orientDB/backend/app.js
+++ orientDB/backend/app.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/public/main.html');
+});
+
+app.get('/main', function (req, res) {
+    res.sendFile(__dirname + '/public/main.html');
+});
+
+app.post('/email_post', function (req, res) {
+    console.log(req.body.email);
+    //res.send('<h1>welcome!' + req.body.email + '</h1>');
+    res.render('email.ejs', { 'email': req.body.email });
 });
 
 app.listen(3000, function () {
