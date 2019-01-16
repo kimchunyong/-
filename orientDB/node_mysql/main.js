e636bcc56c315d7df954ce104ab6cd58cd10ab51
Kimchunyong <lovemy5854@naver.com>
2019-01-16 11:48:38 +0900

    test

--- orientDB/node_mysql/main.js
+++ orientDB/node_mysql/main.js
@@ -1,10 +1,7 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
 var db = require('./lib/db');
 
 var app = http.createServer(function (request, response) {
